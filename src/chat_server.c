5688afc27cebe9a90222c7284860ffa93b3a462e
TheStriker <anasrifakofficial@gmail.com>
2026-05-20 12:20:21 +0100

    server prototype complete
    - can now send msg to client
    - removed server_buffer since i use dynamic array (because i use my get_string() to store server input) and it would be redundant to copy dynamic arrays content to server buffer so might as well use that
    - added boolean to terminate session from the server with /exit, similar to client who just breaks outside of loop
    - at the moment the fork is useless since they are all hooked to the same terminal meaning i can't chat with multiple clients but will see what to do

--- src/chat_server.c
+++ src/chat_server.c
@@ -8,6 +8,7 @@
 
 #define DEFAULT_QUEUE_LENGTH 10
 #define MAX_Q_LENGTH 128
+#define MAX_BUFFER_SIZE 1024
 
 #define BAD_PORT -10
 #define SOCKET_FAILED -11
@@ -16,12 +17,17 @@
 #define BAD_Q_LENGTH -14
 #define LISTEN_FAILED -15
 #define FORK_FAILED -16
+#define MALLOC_FAILED -17
 
+
+//MARK:PROTOTYPES
 int get_port(const char* str);
 int get_q_length(const char* str);
 void chat_service(int sd, struct sockaddr_in client_addr);// will define later
 char *get_string(const char* prompt);
 
+
+//MARK:MAIN
 int main(int argc, char** argv) {
     int queue_length = DEFAULT_QUEUE_LENGTH;
     int server_port;
@@ -94,54 +100,82 @@ int main(int argc, char** argv) {
 
     }
 
-
     close(sd);
 
     return 0;
 }
 
+
+//MARK: DEFINITIONS
 void chat_service(int connection_sd, struct sockaddr_in client_addr) {
-    char server_buffer[1024], client_buffer[1024];//client_buffer is our receive_buff && server_buffer is our send_buff 
+    char client_buffer[MAX_BUFFER_SIZE];//client_buffer is our receive_buff
+    char* msg_to_send;
+    int msg_length;
     int client_port;
     char client_ip_str[INET_ADDRSTRLEN];//255.255.255.255 is 15 characters + \0 is 16
-    int nbr_of_bytes_read, nbr_of_bytes_sent;
-    
+    int bytes_read, bytes_sent;
+    bool terminate_session = false;//added this due to server input (look from 145)
 
-    
     inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip_str ,INET_ADDRSTRLEN);//function that takes the binary form the the address and turns it into string
     //it's reverse is inet_pton() 
-    client_port = ntohs(client_addr.sin_port);
+    client_port = ntohs(client_addr.sin_port);//turns big endian back to little endian (forgot to add this last commit)
     printf("Client connected [IP: %s | PORT: %d ]\nTIP:'/exit' to end session\n", client_ip_str, client_port);
     //here i need to specifiy or implement the chatting feature :|
 
-
-    do {
-
+    while(true) {
         //receiving client message, i think i will make client only send 1023 length msgs
+        bytes_read = recv(connection_sd, client_buffer, MAX_BUFFER_SIZE-1, 0);
         
-        
-        nbr_of_bytes_read = recv(connection_sd, client_buffer, sizeof(client_buffer)-1, 0);
-        client_buffer[nbr_of_bytes_read] = '\0';
-
-        if(nbr_of_bytes_read < 0) {
+        if(bytes_read < 0) {
             printf("recv failed!\n");
             break;//this will exit the loop into close(connection_sd);
         } 
+        //moved this here cuz of potential client_buffer[-1] -> segfault
+        client_buffer[bytes_read] = '\0';//null terminate after last byte received
 
         printf("client: %s\n", client_buffer);
 
         if (!strncmp(client_buffer, "/exit", 5)) {//reads first 5 bytes and doesn't care what's after
             printf("Client ended session\n");
-            break;
+            break;//leave instantly, don' waste time waiting for server side input
         }
 
-        printf("server: "); 
-        
+        //server side communication
+        while(true) {
+            msg_to_send = get_string("Server: ");//remember to free if you reach here
+            msg_length = strlen(msg_to_send);
+            if (msg_length >= MAX_BUFFER_SIZE || msg_length <= 0) {
+                free(msg_to_send);//me no forget :'( so no memory leak
+                printf("message too long! ( 1 <= msg_length <= %d)\n", MAX_BUFFER_SIZE-1);
+                continue;
+            } else {
+                if (!strncmp(msg_to_send, "/exit", 5)) {
+                    free(msg_to_send);//me no forget :'( so no memory leak
+                    printf("Server ended session!\n");
+                    terminate_session = true;
+                    break;
+                }
 
+                //send to client
+                //i will later upgrade this to a version that is able to handle incomplete sends
+                bytes_sent = send(connection_sd, msg_to_send, msg_length+1, 0);//msg_length + 1 cuz of \0, I think i don't need server_buff, 0 flag makes it behave like write
+                if (bytes_sent < 0) {
+                    printf("send failed");
+                    free(msg_to_send);
+                    terminate_session = true;
+                    break;//exit the current loop
+                } 
+                free(msg_to_send);//free this array for now until next input
+                break;//terminate session will still be false so it will now look at client input once again
+            }
+        }
 
-    } while (true);
+        if(terminate_session) {
+            break;//leave while loop
+        }
+
+    };
     
-
     close(connection_sd);//close connection socket
 }
 
@@ -194,5 +228,23 @@ int get_q_length(const char* str) {
 }
 
 char *get_string(const char* prompt) {
-    
+    char *str = (char*)malloc(sizeof(char));//for '\0'
+    char *temp = NULL;
+    int c;
+    int size = 1;
+    printf("%s", prompt);
+
+    while ((c = getchar()) != '\n' ) {
+        str[size-1] = c;
+        temp = (char*)realloc(str ,sizeof(char)*(size+1));
+        if (temp == NULL) {
+            free(str);//free the memory cuz it turns out realloc doesn't free the memory if it fails aka memory leak
+            perror("malloc");
+            exit(MALLOC_FAILED);//will kill child process and terminate session (idk if I should kill the parent or no)
+        }
+        str = temp;//now assign it to str
+        size++;
+    }
+    str[size-1] = '\0';
+    return str;
 }
