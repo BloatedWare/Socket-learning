5f8adc28115158ec78ddee13b34d1698eb840611
TheStriker <anasrifakofficial@gmail.com>
2026-05-20 11:01:38 +0100

    server can now store client info and recieve and display them
    -program stores the address and port of the client.
    -started implementing chat_service() that runs in child process.
    -removed buffer from main and created 2 buffers for send_buff and recv_buff in chat_service() instead.
    -moved client info from child process' main to chat_service().
    -is able to receive up to 1023 bytes at a time (recv_buff == client_buffer).
    TODO:
     -make limit on client to only be able to send 1023 bytes of data and ignore if there is more
     -apply same limitation on server side as i don't know how to make recv_buff take all bytes from client (i want to keep blocking enabled)

--- src/chat_server.c
+++ src/chat_server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h> //added this just for aesthetics 
 #include <sys/socket.h>
 #include <unistd.h>
@@ -18,17 +19,17 @@
 
 int get_port(const char* str);
 int get_q_length(const char* str);
-void chats();// will define later
+void chat_service(int sd, struct sockaddr_in client_addr);// will define later
+char *get_string(const char* prompt);
 
 int main(int argc, char** argv) {
     int queue_length = DEFAULT_QUEUE_LENGTH;
-    int server_port, client_port;
+    int server_port;
     struct sockaddr_in server_address;
+
+    
     struct sockaddr_in client_address;//to capture client address
     socklen_t client_struct_len = sizeof(client_address);//need as well for accept
-    char client_ip_str[INET_ADDRSTRLEN];//255.255.255.255 is 15 characters + \0 is 16
-    char buffer[1024];
-    
     
 
     if (argc < 2 || argc > 3) {
@@ -81,10 +82,9 @@ int main(int argc, char** argv) {
                     //same reason as the above, no need to kill the server yet for 1 failed connection
                     break;
                 case 0: 
-                    inet_ntop(AF_INET, &(client_address.sin_addr), client_ip_str ,INET_ADDRSTRLEN);
-                    client_port = ntohs(client_address.sin_port);
-                    printf("Client connected [IP: %s | PORT: %d ]\n", client_ip_str, client_port);
-                    //here i need to specifiy or implement the chatting feature :|
+
+                    
+                    chat_service(connection_sd, client_address);
                     break;
                 default:// I am parent
                     printf("Connection established! :)\n");
@@ -98,6 +98,51 @@ int main(int argc, char** argv) {
     close(sd);
 
     return 0;
+}
+
+void chat_service(int connection_sd, struct sockaddr_in client_addr) {
+    char server_buffer[1024], client_buffer[1024];//client_buffer is our receive_buff && server_buffer is our send_buff 
+    int client_port;
+    char client_ip_str[INET_ADDRSTRLEN];//255.255.255.255 is 15 characters + \0 is 16
+    int nbr_of_bytes_read, nbr_of_bytes_sent;
+    
+
+    
+    inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip_str ,INET_ADDRSTRLEN);//function that takes the binary form the the address and turns it into string
+    //it's reverse is inet_pton() 
+    client_port = ntohs(client_addr.sin_port);
+    printf("Client connected [IP: %s | PORT: %d ]\nTIP:'/exit' to end session\n", client_ip_str, client_port);
+    //here i need to specifiy or implement the chatting feature :|
+
+
+    do {
+
+        //receiving client message, i think i will make client only send 1023 length msgs
+        
+        
+        nbr_of_bytes_read = recv(connection_sd, client_buffer, sizeof(client_buffer)-1, 0);
+        client_buffer[nbr_of_bytes_read] = '\0';
+
+        if(nbr_of_bytes_read < 0) {
+            printf("recv failed!\n");
+            break;//this will exit the loop into close(connection_sd);
+        } 
+
+        printf("client: %s\n", client_buffer);
+
+        if (!strncmp(client_buffer, "/exit", 5)) {//reads first 5 bytes and doesn't care what's after
+            printf("Client ended session\n");
+            break;
+        }
+
+        printf("server: "); 
+        
+
+
+    } while (true);
+    
+
+    close(connection_sd);//close connection socket
 }
 
 int get_port(const char* str) {
@@ -146,4 +191,8 @@ int get_q_length(const char* str) {
     
 
     return q_length; 
+}
+
+char *get_string(const char* prompt) {
+    
 }
