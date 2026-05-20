6694ecf7dcc99bbdcef674b4e1e33301dcfd53de
TheStriker <anasrifakofficial@gmail.com>
2026-05-20 02:21:54 +0100

    updated get_port and created get_queue length for the listen() function
    -get_port() now exits upon failure displaying cause rather than being handled in main
    -get_queue() copies same behaviour but for queue length
    NOTE: you can change the max queue length through the MAX_Q_LENGTH macro

--- src/chat_server.c
+++ src/chat_server.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define DEFAULT_QUEUE_LENGTH 10
+#define MAX_Q_LENGTH 128
 
 #define BAD_PORT -10
 #define SOCKET_FAILED -11
 #define INSUFFICIENT_ARGS -12
+#define BIND_FAILED -13
+#define BAD_Q_LENGTH -14
 
 int get_port(const char* str) {
     int port;
@@ -16,30 +21,66 @@ int get_port(const char* str) {
     str_head = str;//point to first non white space char
 
     do { //will return false for (+/-) which i want
-        if (!(*str >= '0' && *str <= '9')) return BAD_PORT;
+        if (!(*str >= '0' && *str <= '9')) {
+            printf("Port: Not a number!\n");
+            exit(BAD_PORT);
+        }
         str++;
     } while (*str != '\0');
     port = atoi(str_head);
 
-    if (port < 1024 || port > 65535) return BAD_PORT; //we are only allowed from 1024 and up
+    if (port < 1024 || port > 65535) {
+        printf("Port: port must be within [1024, 65535]!\n");
+        exit(BAD_PORT); //we are only allowed from 1024 and up
+    }
     return port;
+}
+
+int get_q_length(const char* str) {
+   int q_length;
+    const char* str_head;
+    if (str == NULL) return BAD_PORT;
+    while (*str == ' ') str++;//skip white space
+    str_head = str;//point to first non white space char
+
+    do { //will return false for (+/-) which i want
+        if (!(*str >= '0' && *str <= '9')) {
+            printf("queue length: not a number!\n");
+            exit(BAD_Q_LENGTH);
+        } 
+        str++;
+    } while (*str != '\0');
+    q_length = atoi(str_head);
+
+    if (q_length < 0 || q_length > MAX_Q_LENGTH) {
+        printf("queue length must be non negative and musn't surpass %d!\n", MAX_Q_LENGTH);
+        exit (BAD_Q_LENGTH); //we are only allowed from 1024 and up
+    }
+    
+
+    return q_length; 
 }
 
 void chats();
 
 int main(int argc, char** argv) {
-
+    int queue_length = DEFAULT_QUEUE_LENGTH;
     int port;
     struct sockaddr_in address;
     char buffer[1024];
 
-    if (argc != 2) {
-        printf("Usage: %s <port-number>\n", argv[0]);
+    if (argc < 2 || argc > 3) {
+        printf("Usage: %s <port-number> [queue-length=%d]\n", argv[0], queue_length);
         exit(INSUFFICIENT_ARGS);
     }
+    
     port = get_port(argv[1]);
-    printf("port: %d\n", port);
 
+    if (argc == 3) {
+        queue_length = get_q_length(argv[2]);
+    } 
+
+    printf("port: %d, q_l= %d\n", port, queue_length);
 
     // int sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
@@ -48,9 +89,15 @@ int main(int argc, char** argv) {
     //     exit(SOCKET_FAILED);
     // }
 
-    // address.sin_family = AF_INET;
-    // address.sin_port = htons();
 
+    // address.sin_family = AF_INET;
+    // address.sin_port = htons((u_int16_t)port);// port must be 16 bits AND in big endian, which our system stores in little endian
+    // address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);//macro for 127.0.0.1 in binary and htonl to convert to big endian (same as port)
+
+    // if (bind(sd, (struct sockaddr*)&address, sizeof(address)) != 0) {
+    //     perror("bind");
+    //     exit(BIND_FAILED);
+    // }
 
     return 0;
 }
